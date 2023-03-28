75e49c0091148a028e3df3a00609cfeaaa0211ee
elias <eliasgamez94@gmail.con>
2023-03-27 20:20:45 -0600

    Subiendo  las correciones del CAMBIOS EN CSS Y JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,6 @@
+
+//Creamos la funcion junto con sus variables 
+
 document.addEventListener("DOMContentLoaded", function () {
   const generateBtn = document.getElementById("generate");
   const passwordTextArea = document.getElementById("password");
@@ -7,9 +10,16 @@ document.addEventListener("DOMContentLoaded", function () {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
+
+  //Este bloque de código define una función que devuelve un número entero aleatorio entre dos valores. La función toma dos argumentos: min y max, que son los valores mínimo y máximo de la serie de números aleatorios generados.
   function generatePassword(passwordLength) {
     const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
     let password = "";
+
+    /*Este bloque de código define una función que toma un argumento passwordLength, que es el número de 
+    caracteres que se desea en la contraseña. La función devuelve una cadena aleatoria de caracteres 
+    alfanuméricos y especiales de la longitud especificada. La función utiliza un bucle for para iterar 
+    passwordLength veces y en cada iteración elige un carácter aleatorio de la cadena de caracteres. */
 
     for (let i = 0; i < passwordLength; i++) {
       const randomIndex = getRandomInt(0, characters.length - 1);
@@ -26,3 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+/**Este bloque de código establece un evento click en el botón generateBtn. Cuando se hace clic en el botn, 
+ * se llama a la función generatePassword() con el número de caracteres deseados obtenidos del elemento 
+ * passwordLengthInput. 
+ * Lego, la nueva contraseña generada se establece como el valor del textarea paswordTextArea. */
